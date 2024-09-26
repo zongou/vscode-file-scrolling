ca596b0bfa7bfa706d377e61c0c807fc08e67012
zongou <zongou@outlook.com>
2024-09-26 09:36:20 +0800

    better logic

--- src/extension.ts
+++ src/extension.ts
@@ -1,27 +1,27 @@
 import * as vscode from 'vscode';
 
 const extId = 'file-auto-scrolling';
-const filePositionMap = new Map();
+const fileListEnabled = new Set();
 
 export function activate(context: vscode.ExtensionContext) {
 	context.subscriptions.push(vscode.commands.registerCommand(`${extId}.toggle`, () => {
 		const editor = vscode.window.activeTextEditor;
 		if (editor) {
-			/* Prevent cursor from moving down when its at the document end by moving it 1 line up */
-			const lastLine = editor.document.lineCount - 1;
-			if (lastLine != 0 && editor.selection.active.line === lastLine) {
-				const newPosition = new vscode.Position(lastLine - 1, 0);
-				editor.selection = new vscode.Selection(newPosition, newPosition);
-			}
-
 			const filePath = editor.document.uri.path;
-			filePositionMap.has(filePath) ? filePositionMap.delete(filePath) : filePositionMap.set(filePath, editor.selection.active);
+			if (!fileListEnabled.has(filePath)) {
+				const lastLine = editor.document.lineCount - 1;
+				const lastPosition = new vscode.Position(lastLine, editor.document.lineAt(lastLine).text.length);
+				editor.selection = new vscode.Selection(lastPosition, lastPosition);
+				fileListEnabled.add(filePath);
+			} else {
+				fileListEnabled.delete(filePath);
+			}
 		}
-	}))
+	}));
 
 	context.subscriptions.push(vscode.workspace.onDidChangeTextDocument(event => {
 		const filePath = event.document.uri.path;
-		if (filePositionMap.has(filePath)) {
+		if (fileListEnabled.has(filePath)) {
 			const editor = vscode.window.visibleTextEditors.find(editor => editor.document.uri.path === filePath);
 			if (editor) {
 				const lastLine = editor.document.lineCount - 1;
@@ -32,17 +32,12 @@ export function activate(context: vscode.ExtensionContext) {
 	}));
 
 	context.subscriptions.push(vscode.window.onDidChangeTextEditorSelection(event => {
-		const filePath = event.textEditor.document.uri.path;
-		if (filePositionMap.has(filePath)) {
-			const savedPosition = filePositionMap.get(filePath);
-			const currentPosition = event.selections[0].active;
-			if (savedPosition.line !== currentPosition.line || savedPosition.character !== currentPosition.character) {
-				filePositionMap.delete(filePath);
-			}
+		if (event.selections[0].active.line !== event.textEditor.document.lineCount - 1) {
+			fileListEnabled.delete(event.textEditor.document.uri.path);
 		}
 	}));
 
 	context.subscriptions.push(vscode.workspace.onDidCloseTextDocument(doc => {
-		filePositionMap.delete(doc.uri.path);
+		fileListEnabled.delete(doc.uri.path);
 	}));
 }
